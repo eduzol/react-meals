6eaa88fcadf9c63f110188c1e827d15226cf536f
EduardoZola <eduzol@gmail.com>
2017-09-07 02:12:36 -0700

    install Provider Component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import reducer from './reducers';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer, 
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
@@ -12,5 +13,7 @@ const store = createStore(reducer,
 
 console.log(store.getState());
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}> 
+                    <App/> 
+                </Provider>, document.getElementById('root'));
 registerServiceWorker();
